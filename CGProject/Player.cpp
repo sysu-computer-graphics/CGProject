464c07a4a9000a93f685a29a44537d85b24e3791
CarolSum <905965287@qq.com>
2019-06-13 16:36:03 +0800

    fix gun model fixed on the screen

--- CGProject/Player.cpp
+++ CGProject/Player.cpp
@@ -39,23 +39,23 @@ void Player::setRotate(float radians) {
 }
 
 
-void Player::render()
+void Player::render(glm::mat4 view)
 {
 	// don't forget to enable shader before setting uniforms
 	this->shader->use();
 
 	// view/projection transformations
 	glm::mat4 projection = glm::perspective(glm::radians(Controler::camera.getZoom()), (float)Controler::getInstance()->getScrWidth() / (float)Controler::getInstance()->getScrHeight(), 0.1f, 100.0f);
-	glm::mat4 view = Controler::camera.getViewMatrix();
 	this->shader->setMat4("projection", projection);
 	this->shader->setMat4("view", view);
 
 	// render the loaded model
 	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::translate(model, this->position);
-	// model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
-	model = glm::rotate(model, this->radians, glm::vec3(0.0f, 1.0f, 0.0f));
-	model = glm::scale(model, glm::vec3(1.5f, 1.5f, 1.5f));	// it's a bit too big for our scene, so scale it down
+	model = glm::translate(model, glm::vec3(-0.15f, -2.2f, 1.6f));
+	model = glm::rotate(model, glm::radians(10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	model = glm::rotate(model, glm::radians(-4.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
+
 	this->shader->setMat4("model", model);
 	this->model->draw(*this->shader);
 }
